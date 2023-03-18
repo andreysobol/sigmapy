797dd32be0d8b5dc4002ba48a18d9c9115ee6006
Oles Holembovskyy <oh@matterlabs.dev>
2023-03-18 17:26:51 +0000

    simple exponent proofs

---
+++ sigma_protocols.py
@@ -0,0 +1,34 @@
+from curve import *
+
+class SingleExponentProof:
+    def __init__(
+        gen: Point,
+        gen_x: Point,
+        commitment: Point,
+        response: int,
+    ):
+        self.generator = gen
+        self.pedersen_hash = gen_x
+        self.commitment = commitment
+        self.responce = response
+
+def generate_single_exponent_proof(
+    value: int,
+    generator: Point,
+    random_value: int,
+) -> SingleExponentProof:
+    pedersen_hash = point_mul(generator, value)
+    commitment = point_mul(generator, random_value)
+
+    seed = bytes_from_point(pedersen_hash) + bytes_from_point(commitment)
+    challenge = hash_sha256(seed)
+
+    responce = value * challenge + random_value
+
+    return SingleExponentProof(
+        generator,
+        pedersen_hash,
+        commitment,
+        responce
+    )
+
