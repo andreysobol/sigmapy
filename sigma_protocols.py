0d98efaa246bf84f0c9471e3eff75fda20e850a1
Oles Holembovskyy <oh@matterlabs.dev>
2023-03-18 18:14:28 +0000

    exponent equality proofs

--- sigma_protocols.py
+++ sigma_protocols.py
@@ -48,3 +48,49 @@ def verify_single_exponent_proof(
     right = point_mul(proof.generator, proof.responce) 
 
     return left == right
+
+class SingleExponentEqualityProof:
+    def __init__(
+        gen_1: Point,
+        gen_1_x: Point,
+        gen_2: Point,
+        gen_2_x: Point,
+        commitment_1: Point,
+        commitment_2: Point,
+        response: int,
+    ):
+        self.generator_1 = gen_1
+        self.generator_2 = gen_2
+        self.pedersen_hash_1 = gen_1_x
+        self.pedersen_hash_2 = gen_2_x
+        self.commitment_1 = commitment_1
+        self.commitment_2 = commitment_2
+        self.responce = response
+
+def generate_single_exponent_equality_proof(
+    value: int,
+    generator_1: Point,
+    generator_2: Point,
+    random_value: int,
+) -> SingleExponentEqualityProof:
+    pedersen_hash_1 = point_mul(generator_1, value)
+    pedersen_hash_2 = point_mul(generator_2, value)
+
+    commitment_1 = point_mul(generator_1, random_value)
+    commitment_2 = point_mul(generator_2, random_value)
+
+    seed = bytes_from_point(pedersen_hash_1) + bytes_from_point(pedersen_hash_2) + bytes_from_point(commitment_1) + bytes_from_point(commitment_2)
+    challenge_bytes = hash_sha256(seed)
+    challenge = int_from_bytes(challenge_bytes) % n
+
+    responce = (value * challenge + random_value) % n
+
+    return SingleExponentEqualityProof(
+        generator_1,
+        generator_2,
+        pedersen_hash_1,
+        pedersen_hash_2,
+        commitment_1,
+        commitment_2,
+        responce
+    )
