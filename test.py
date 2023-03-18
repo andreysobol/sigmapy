9cdb92b03fdedacf7ab6a5aad7ac87e90d29d479
Andrey Sobol <andreysobol@protonmail.com>
2023-03-18 18:01:11 +0000

    pedersen_commitment test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from tests.single_exponent_test import TestSignleExponent
+from tests.pedersen_commitment_test import TestPedersenCommitment
 
 if __name__ == '__main__':
     unittest.main()
