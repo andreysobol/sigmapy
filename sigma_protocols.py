eb175d9044bdef3afdb9438fa30173bca72405dd
Oles Holembovskyy <oh@matterlabs.dev>
2023-03-18 18:55:46 +0000

    pedersen inner product

--- sigma_protocols.py
+++ sigma_protocols.py
@@ -1,4 +1,5 @@
 from curve import *
+from pedersen_commitment import *
 
 class SingleExponentProof:
     def __init__(
@@ -118,3 +119,88 @@ def verify_single_exponent_equality_proof(
     right2 = point_mul(proof.generator_2, proof.responce)
 
     return (left1 == right1) and (left2 == right2)
+
+# Prove that b / a = d / c
+class SinglePedersenInnerProductProof:
+    def __init__(
+        self,
+        gen_1: Point,
+        gen_2: Point,
+        comm_a: Point,
+        comm_b: Point,
+        comm_c: Point,
+        comm_d: Point,
+        commitment_1: Point,
+        commitment_2: Point,
+        response: int,
+        inner_proof_1: SingleExponentProof,
+        inner_proof_2: SingleExponentProof
+    ):
+        assert(inner_proof_1.generator == gen_2)
+        assert(inner_proof_2.generator == gen_2)
+
+        self.generator_1 = gen_1
+        self.generator_2 = gen_2
+        self.pedersen_commitment_a = comm_a
+        self.pedersen_commitment_b = comm_b
+        self.pedersen_commitment_c = comm_c
+        self.pedersen_commitment_d = comm_d
+        self.commitment_1 = commitment_1
+        self.commitment_2 = commitment_2
+        self.responce = response
+        self.inner_proof_1 = inner_proof_1
+        self.inner_proof_2 = inner_proof_2
+
+def generate_single_pedersen_inner_product_proof(
+    a: int,
+    b: int,
+    c: int,
+    d: int,
+    r1: int,
+    r2: int,
+    r3: int,
+    r4: int,
+    random_value: int,
+    inner_random_value_1: int,
+    inner_random_value_2: int,
+) -> SinglePedersenInnerProductProof:
+    pedersen_comm_a = pedersen_commitment(a, r1)
+    pedersen_comm_b = pedersen_commitment(b, r2)
+    pedersen_comm_c = pedersen_commitment(c, r3)
+    pedersen_comm_d = pedersen_commitment(d, r4)
+
+    commitment_1 = point_mul(G, a * random_value)
+    commitment_2 = point_mul(G, c * random_value)
+
+    seed = bytes_from_point(pedersen_comm_a) + bytes_from_point(pedersen_comm_b) 
+    seed += bytes_from_point(pedersen_comm_c) + bytes_from_point(pedersen_comm_d)
+    seed += bytes_from_point(commitment_1) + bytes_from_point(commitment_2)
+    
+    challenge_bytes = hash_sha256(seed)
+    challenge = int_from_bytes(challenge_bytes) % n
+
+    value = (pow(a, n-2, n) * b) % n
+    value_2 = (pow(c, n-2, n) * d) % n
+    assert(value == value_2)
+
+    responce = (value * challenge + random_value) % n
+
+    inner_exponent_1 = (responce * r1 - challenge * r2) % n
+    inner_exponent_2 = (responce * r3 - challenge * r4) % n
+
+    inner_proof_1 = generate_single_exponent_proof(inner_exponent_1, H, inner_random_value_1)
+    inner_proof_2 = generate_single_exponent_proof(inner_exponent_2, H, inner_random_value_2)
+
+
+    return SinglePedersenInnerProductProof(
+        G, H,
+        pedersen_comm_a,
+        pedersen_comm_b,
+        pedersen_comm_c,
+        pedersen_comm_d,
+        commitment_1,
+        commitment_2,
+        responce,
+        inner_proof_1,
+        inner_proof_2
+    )
