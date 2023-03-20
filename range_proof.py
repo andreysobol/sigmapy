475d2167ba1961bf0e539f8504152f578d09e476
Andrey Sobol <andreysobol@protonmail.com>
2023-03-20 23:39:50 +0000

    verify_range_proof

--- range_proof.py
+++ range_proof.py
@@ -9,7 +9,7 @@ import math
 from itertools import reduce
 from typing import List
 
-from boolean_proof import generate_boolean_proof
+from boolean_proof import generate_boolean_proof, verify_boolean_proof
 from curve import G, H, Point, point_mul, point_add
 from pedersen_commitment import pedersen_commitment
 from sigma_protocols import *
@@ -29,12 +29,10 @@ class RangeProof:
     def __init__(
         self,
         main_commitment,
-        witness_bit_commitments: List[Point],
         boolean_proofs: List[SinglePedersenInnerProductProof],
         final_inner_proof: SingleExponentProof,
     ):
         self.main_commitment = main_commitment
-        self.witness_bit_commitments = witness_bit_commitments
         self.boolean_proofs = boolean_proofs
         self.final_inner_proof = final_inner_proof
 
@@ -84,13 +82,45 @@ def generate_range_proof(
         blinding_factor_for_value_commitment
     )
 
-    witness_bit_commitments = [boolean_proofs[i].pedersen_commitment_b for i in range(power_of_two)]
-
     range_proof = RangeProof(
         main_commitment,
-        witness_bit_commitments,
         boolean_proofs,
         inner_proof,
     )
 
     return range_proof
+
+def verify_range_proof(
+    generator_1: Point,
+    generator_2: Point,
+    proof: RangeProof,
+    n: int,
+) -> bool:
+    power_of_two = get_power_of_two(n)
+
+    if power_of_two != len(proof.boolean_proofs):
+        return False
+
+    if not verify_single_exponent_proof(
+        generator_2,
+        proof.final_inner_proof,
+    ):
+        return False
+    
+    for i in range(power_of_two):
+        if not verify_boolean_proof(
+            generator_1,
+            generator_2,
+            proof.boolean_proofs[i],
+        ):
+            return False
+
+    witness_bit_commitments = [proof.boolean_proofs[i].pedersen_commitment_b for i in range(power_of_two)]
+
+    witness_bit_commitments_mul = [point_mul(witness_bit_commitments[i], 2**i) for i in range(power_of_two)]
+    witness_bit_commitments_sum = reduce(lambda x,y: point_add(x,y), witness_bit_commitments_mul[1:], witness_bit_commitments[0])
+
+    if point_sub(witness_bit_commitments_sum, proof.main_commitment) != proof.final_inner_proof.exponent:
+        return False
+
+    return True
