f2f33d84534dc601f41f9955d9b0196433efaecd
Andrey Sobol <andreysobol@protonmail.com>
2023-03-20 00:19:39 +0000

    test py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from tests.pedersen_commitment_test import TestPedersenCommitment
 from tests.single_exponent_equality_test import TestSignleExponentEquality
 from tests.single_pedersen_inner_product_test import TestSinglePedersenInnerProduct
 from tests.boolean_test import TestBoolean
+from tests.polynomial_commitment_test import TestPolinomialCommitment
 
 if __name__ == '__main__':
     unittest.main()
